c1c6f57cd110c28a7f6d2424125cc99ff23d3342
msahrana <msahrana@gmail.com>
2023-11-08 00:16:37 +0600

    get section is created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -23,7 +23,28 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
-    await client.connect();
+    // await client.connect();
+
+    const foodCollection = client.db('foodDB').collection('food');
+
+    app.post('/food', async(req, res)=>{
+      const newFood = req.body;
+      console.log(newFood)
+      const result = await foodCollection.insertOne(newFood)
+      res.send(result)
+    })
+
+    app.get('/food', async(req, res) => {
+      const result = await foodCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/food/:id', async(req, res) => {
+      const id = req.params.id 
+      const query = {_id : new ObjectId(id)}
+      const result = await foodCollection.findOne(query)
+      res.send(result)
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
