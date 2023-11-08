ee2d8225572ef248ed64198cdb00474a6e0e1710
msahrana <msahrana@gmail.com>
2023-11-08 15:34:54 +0600

    delete option is added successfully

--- index.js
+++ index.js
@@ -48,6 +48,27 @@ async function run() {
       res.send(result)
     })
 
+    // app.patch('/food/:id', async(req, res)=>{
+    //   const id = req.params.id;
+    //   const filter = {_id : new ObjectId(id)};
+    //   const updateFood = req.body;
+    //   console.log(updateFood);
+    //   const updateDoc = {
+    //     $set: {
+    //       status: updateFood.status
+    //     },
+    //   };
+    //   const result = await foodCollection.updateOne(filter, updateDoc);
+    //   res.send(result);
+    // })
+
+    app.delete('/food/:id', async(req, res) => {
+      const id = req.params.id 
+      const query = {_id : new ObjectId(id)}
+      const result = await foodCollection.deleteOne(query)
+      res.send(result)
+    })
+
     app.post('/user', async(req, res) => {
       const user = req.body 
       console.log(user)
