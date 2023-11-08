c246012b75063565938283a8e5164b0d71be6994
msahrana <msahrana@gmail.com>
2023-11-08 18:33:12 +0600

    update some code

--- index.js
+++ index.js
@@ -41,6 +41,11 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/request', async(req, res) => {
+      const result = await newFoodCollection.find().toArray();
+      res.send(result);
+    })
+
     app.get('/food/:id', async(req, res) => {
       const id = req.params.id 
       const query = {_id : new ObjectId(id)}
